8be61052af6e10432ad64267e9c21b22d545d84b
Simona <monisimi99@gmail.com>
2021-02-22 22:50:09 +0200

    feat: adding species

--- src/app/Application.js
+++ src/app/Application.js
@@ -1,5 +1,6 @@
 import config from '../config';
 import EventEmitter from 'eventemitter3';
+import StarWarsUniverse from './custom/StarWarsUniverse';
 
 const EVENTS = {
   APP_READY: 'app_ready',
@@ -31,7 +32,9 @@ export default class Application extends EventEmitter {
    */
   async init() {
     // Initiate classes and wait for async operations here.
-
+    const starWarsUniverse = new StarWarsUniverse();
+    await starWarsUniverse.createSpecies();
+    this.data.universe = starWarsUniverse;
     this.emit(Application.events.APP_READY);
   }
 }
